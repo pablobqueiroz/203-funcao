02479d662aeb003e6e7aa080542feb16b670090d
unknown <queirozpablob@gmail.com>
2019-06-12 16:53:37 -0300

    adicionar fucoes

---
+++ script.js
@@ -0,0 +1,33 @@
+// let materialescada = '#';
+// let qntd= Number(prompt("opa, quantos degraus"));
+
+// for(let i=0; i<qntd; i++){
+//     console.log(materialescada);
+//     materialescada = materialescada + degrau;
+// }
+
+// let filmes = [
+//     {titulo:"duro de matar", idade:12},
+//     {titulo:"cidade de deus", idade:18},
+//     {titulo:"colombiana", idade:16}
+// ]
+// let valor= prompt("digite o primeiro valor");
+
+// function somer(n1,n2,n3,n4){
+//     let media = (n1+n2+n3+n4)/4;
+//     console.log(media);
+// }
+// somar(9,8,7,10);
+
+// let idade = prompt("qual é sua idade");
+// if(idade < 17){
+
+
+// if(filmes[0].idade ){
+//     console.log(filmes.titulo)
+// }
+function calc(a,b,c){
+
+    console.log(a,b,c);
+} 
+calc(1,'/',2);
